e244dd20f9e54b381d998f0e4692e23d09deed83
Raoul Rubien <raoul.rubienr@gmail.com>
2021-11-28 22:57:11 +0100

    simple sand clock on secondary disp.: inverts a px every sec, beeps every 1h

--- keyboards/handwired/dactyl_manuform/7x6/keymaps/rubienr/keymap.c
+++ keyboards/handwired/dactyl_manuform/7x6/keymaps/rubienr/keymap.c
@@ -3,48 +3,62 @@
 #include <split_util.h>
 
 #define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
+#define _R_MO   1
+#define _L_MO   2
 
-#define RAISE MO(_RAISE)
-#define LOWER MO(_LOWER)
+#define R_MO MO(_R_MO)
+#define L_MO MO(_L_MO)
+
+static uint16_t last_timer;
+static float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_7x6(
-        KC_ESC,  KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                           KC_F6 , KC_F7 , KC_F8   , KC_F9  , KC_F10  , KC_F11    ,
-        KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                           KC_6  , KC_7  , KC_8    , KC_9   , KC_0    , KC_BSPC   ,
-        KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                           KC_Y  , KC_U  , KC_I    , KC_O   , KC_P    , KC_MINS   ,
-        KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                           KC_H  , KC_J  , KC_K    , KC_L   , KC_SCLN , KC_QUOT   ,
-        KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                           KC_N  , KC_M  , KC_COMM , KC_DOT , KC_SLSH , KC_BSLASH ,
-                         KC_LBRC,KC_RBRC,                                                          KC_PLUS , KC_EQL ,
-                                         RAISE,  KC_SPC,                           KC_ENT, LOWER,
-                                                 KC_TAB,KC_HOME,          KC_END,  KC_DEL,
-                                                 KC_BSPC, KC_GRV,         KC_LGUI, KC_LALT
+        KC_ESC  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   ,                                          KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  ,
+        KC_CIRC , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    ,                                          KC_7    , KC_8    , KC_9    , KC_0    , KC_BSPC ,
+        KC_TAB  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    ,                                          KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_BSPC ,
+        KC_LSFT , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    ,                                          KC_H    , KC_J    , KC_K    , KC_L    , XXXXXXX , XXXXXXX ,
+        KC_LCTL , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    ,                                          KC_N    , KC_M    , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+                            KC_LPRN , KC_LBRC ,                                                                                  KC_RBRC , KC_RPRN ,
+                                                KC_LALT , KC_SPC  ,                                          KC_ENT  , KC_RCTL ,
+                                                                    KC_DEL  , L_MO    ,            R_MO    , KC_DEL  ,
+                                                                    XXXXXXX , XXXXXXX ,            XXXXXXX , XXXXXXX
     ),
 
-    [_LOWER] = LAYOUT_7x6(
-        KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6  ,                         KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,KC_F12 ,
-        KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
-        _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
-        _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
-        _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
-                                                _______,KC_PSCR,            _______, KC_P0,
-                                                _______,_______,            _______,_______,
-                                                _______,_______,            _______,_______,
-                                                _______,_______,            _______,_______
+    [_R_MO] = LAYOUT_7x6(
+        TERM_OFF, TERM_ON , _______ , _______ , _______ , _______ ,                                          XXXXXXX , KC_PSCR , KC_SLCK , KC_PAUS , KC_F11  , KC_F12  ,
+        _______ , _______ , _______ , _______ , _______ , _______ ,                                          XXXXXXX , KC_INS  , KC_HOME , KC_PGUP , XXXXXXX , XXXXXXX ,
+        _______ , _______ , _______ , _______ , _______ , _______ ,                                          XXXXXXX , KC_DEL  , KC_END  , KC_PGDN , XXXXXXX , XXXXXXX ,
+        _______ , _______ , _______ , _______ , _______ , _______ ,                                          XXXXXXX , XXXXXXX , KC_UP   , XXXXXXX , XXXXXXX , XXXXXXX ,
+        _______ , _______ , _______ , _______ , _______ , _______ ,                                          XXXXXXX , KC_LEFT , KC_DOWN , KC_RIGHT, XXXXXXX , RESET   ,
+                            _______ , _______ ,                                                                                  XXXXXXX , XXXXXXX ,
+                                                _______ , _______ ,                                          _______ , _______ ,
+                                                                    _______ , _______ ,            _______ , _______ ,
+                                                                    _______ , _______ ,            _______ , _______
     ),
 
-    [_RAISE] = LAYOUT_7x6(
-        KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-        _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
-        _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
-        _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
-        _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
-                                                _______,_______,            KC_EQL ,_______,
-                                                _______,_______,            _______,_______,
-                                               _______,_______,            _______,_______,
-                                               _______,_______,            _______,_______
-    )
+    [_L_MO] = LAYOUT_7x6(
+        TERM_OFF, TERM_ON , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                                          _______ , _______ , _______ , _______ , _______ , _______ ,
+        XXXXXXX , XXXXXXX , RGB_SPD , RGB_SPI , XXXXXXX , XXXXXXX ,                                          _______ , _______ , _______ , _______ , _______ , _______ ,
+        XXXXXXX , XXXXXXX , RGB_VAD , RGB_VAI , XXXXXXX , XXXXXXX ,                                          _______ , _______ , _______ , _______ , _______ , _______ ,
+        XXXXXXX , XXXXXXX , RGB_SAD , RGB_SAI , XXXXXXX , XXXXXXX ,                                          _______ , _______ , _______ , _______ , _______ , _______ ,
+        RESET   , RGB_TOG , RGB_HUD , RGB_HUI , XXXXXXX , XXXXXXX ,                                          _______ , _______ , _______ , _______ , _______ , _______ ,
+                            RGB_RMOD, RGB_MOD,                                                                                  _______ , _______ ,
+                                                _______ , _______ ,                                          _______ , _______ ,
+                                                                    _______ , _______ ,            _______ , _______ ,
+                                                                    _______ , _______ ,            _______ , _______
+    ),
+//    [_L_MO] = LAYOUT_7x6(
+//        _______ , _______ , _______ , _______ , _______ , _______ ,                                          _______ , _______ , _______ , _______ , _______ , _______ ,
+//        _______ , _______ , _______ , _______ , _______ , _______ ,                                          _______ , _______ , _______ , _______ , _______ , _______ ,
+//        _______ , _______ , _______ , _______ , _______ , _______ ,                                          _______ , _______ , _______ , _______ , _______ , _______ ,
+//        _______ , _______ , _______ , _______ , _______ , _______ ,                                          _______ , _______ , _______ , _______ , _______ , _______ ,
+//        _______ , _______ , _______ , _______ , _______ , _______ ,                                          _______ , _______ , _______ , _______ , _______ , _______ ,
+//                            _______ , _______ ,                                                                                  _______ , _______ ,
+//                                                _______ , _______ ,                                          _______ , _______ ,
+//                                                                    _______ , _______ ,            _______ , _______ ,
+//                                                                    _______ , _______ ,            _______ , _______
+//    ),
 };
 
 void board_init(void) {
@@ -56,62 +70,101 @@ void board_init(void) {
 #endif
 }
 
-
 #ifdef OLED_ENABLE
 
-void render_status(void) {
-    // Host Keyboard Layer Status
-    oled_write_P(PSTR("Layer: "), false);
+static void render_master_status(void) {
+
+    if (is_keyboard_master()) {
+        oled_write_P(PSTR("Mastr"), false);
+    }
+
+    // layer info
+    oled_write_P(PSTR("\nL:"), false);
+    static char rgb_buffer[10];
 
     switch (get_highest_layer(layer_state)) {
         case _QWERTY:
-            oled_write_P(PSTR("QERTY\n"), false);
+            oled_write_P(PSTR("QWE\n"), false);
             break;
-        case _LOWER:
-            oled_write_P(PSTR("LOWER\n"), false);
+        case _R_MO:
+            oled_write_P(PSTR("RMO\n"), false);
             break;
-        case _RAISE:
-            oled_write_P(PSTR("RAISE\n"), false);
+        case _L_MO:
+            oled_write_P(PSTR("LMO\n"), false);
             break;
         default:
-            // Or use the write_ln shortcut over adding '\n' to the end of your string
-            oled_write_ln_P(PSTR("Undefined"), false);
+            oled_write_P(PSTR("nil\n"), false);
+    }
+    
+    // kbd LED states
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(PSTR("NUM\n"), led_state.num_lock);
+    oled_write_P(PSTR("CAP\n"), led_state.caps_lock);
+    oled_write_P(PSTR("SCR\n"), led_state.scroll_lock);
+
+    // RGB info
+    sprintf(rgb_buffer, PSTR("RGB%02d"), rgblight_get_mode());
+    oled_write(rgb_buffer, rgblight_is_enabled());
+    sprintf(rgb_buffer, PSTR("H:%03d"), rgblight_get_hue());
+    oled_write(rgb_buffer, false);
+    sprintf(rgb_buffer, PSTR("S:%03d"), rgblight_get_sat());
+    oled_write(rgb_buffer, false);
+    sprintf(rgb_buffer, PSTR("V:%03d"), rgblight_get_val());
+    oled_write(rgb_buffer, false);
+    sprintf(rgb_buffer, PSTR("s:%03d"), rgblight_get_speed());
+    oled_write(rgb_buffer, false);
+}
+
+// inverts a pixel every second and moves to next,
+// leaves one out every 15 minutes,
+// beeps every hour and starts from first pixel
+static void render_sandclock(void) {
+    static int x = 0, y = 0, dx=1;
+    static bool px_value = true;
+    static uint16_t elapsed_seconds = 0;
+
+    if (timer_elapsed(last_timer) < 1000) return;
+    last_timer = timer_read();
+    elapsed_seconds++;
+
+    // render pixels, every 15 minutes leave one out 
+    oled_write_pixel(x,y, (0 == (elapsed_seconds % (60 * 15))) ? !px_value : px_value);
+    x += dx;
+
+    // every hour: play a notification, restart from top
+    if (elapsed_seconds > (60*60)) {
+      x = 0;
+      y = 0;
+      dx = 1;
+      px_value = !px_value;
+      elapsed_seconds = 0;
+#ifdef AUDIO_ENABLE
+      PLAY_SONG(tone_qwerty);
+#endif
+      return;
     }
 
-    // Host Keyboard LED Status
-    led_t led_state = host_keyboard_led_state();
-    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
-    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
-    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+    // advance pixel location
+    if (x >= OLED_DISPLAY_HEIGHT || x < 0) {
+        dx *= -1;
+        y++;
+        
+        if (y >= OLED_DISPLAY_WIDTH) {
+          y = 0;
+          px_value = !px_value;
+        }
+    }
 }
-
-
-static void render_logo(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
-    };
-
-    oled_write_P(qmk_logo, false);
-}
-
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (!is_keyboard_left()) {
-        return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-    }
-
+    rotation = OLED_ROTATION_270;
+    last_timer = timer_read();
     return rotation;
 }
 
 void oled_task_user(void) {
-    if (!is_keyboard_left()) {
-        render_status();     // Renders the current keyboard state (layer, lock, caps, scroll, etc)
-    } else {
-        render_logo();       // Renders a static logo
-        oled_scroll_left();  // Turns on scrolling
-    }
+    if (is_keyboard_master()) render_master_status();
+    else render_sandclock();
 }
 
 #endif
