39018d736b0ba39579563af3b8ff4724455c5aef
Raoul Rubien <raoul.rubienr@gmail.com>
2021-12-10 20:33:00 +0100

    refined input method: ({[ brackes with lshift+rshift|ralt|rctl and vice versa; refined input of umlats a then overlapping e is umlaut etc.

--- keyboards/handwired/dactyl_manuform/7x6/keymaps/rubienr/input.c
+++ keyboards/handwired/dactyl_manuform/7x6/keymaps/rubienr/input.c
@@ -1,3 +1,12 @@
+// tap dance input / double tap input
+
+enum {
+    TD_CL,
+    TD_CR,
+    TD_TG_L,
+    TD_TG_R,
+};
+
 // combo input
 
 enum combo_events {
@@ -39,20 +48,142 @@ const qk_ucis_symbol_t ucis_symbol_table[] = UCIS_TABLE(
 );
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    static bool lsft_depressed = false, rsft_depressed = false;
+    static bool lctl_depressed = false, rctl_depressed = false;
+    static bool lalt_depressed = false, ralt_depressed = false;
+    static bool a_depressed = false, o_depressed = false, u_depressed = false;
+    static bool s_depressed = false, e_depressed = false, z_depressed = false;
+
     switch (keycode) {
     case CK_UCIS:
         if (!record->event.pressed) qk_ucis_start();
         break;
+
+    case DE_A:
+        a_depressed = record->event.pressed;
+        break;
+    case DE_O:
+        o_depressed = record->event.pressed;
+        break;
+    case DE_U:
+        u_depressed = record->event.pressed;
+        break;
+    case DE_S:
+        s_depressed = record->event.pressed;
+        break;
+
+    case DE_E: // umlaut: ae, oe, ue --> ä, ö, ü; for example an a then e is more convenient that an a + e combo
+        e_depressed = record->event.pressed;
+        if (e_depressed) {
+            if (a_depressed) {
+                unregister_code(DE_E);
+                register_code(KC_BSPC);
+                unregister_code(KC_BSPC);
+                register_code(DE_ADIA);
+                unregister_code(DE_ADIA);
+                return false;
+            }
+            if (o_depressed) {
+                unregister_code(DE_E);
+                register_code(KC_BSPC);
+                unregister_code(KC_BSPC);
+                register_code(DE_ODIA);
+                unregister_code(DE_ODIA);
+                return false;
+            }
+            if (u_depressed) {
+                unregister_code(DE_E);
+                register_code(KC_BSPC);
+                unregister_code(KC_BSPC);
+                register_code(DE_UDIA);
+                unregister_code(DE_UDIA);
+                return false;
+            }
+        }
+        break;
+
+    case DE_Z: // sz -> ß
+        z_depressed = record->event.pressed;
+        if (s_depressed && z_depressed) {
+            unregister_code(DE_Z);
+            register_code(KC_BSPC);
+            unregister_code(KC_BSPC);
+            register_code(DE_SS);
+            unregister_code(DE_SS);
+            return false;
+        }
+        break;
+
+    case TD(TD_CL): // rsft + lsft --> (
+        lsft_depressed = record->event.pressed;
+        if (lsft_depressed && rsft_depressed) {
+            unregister_code16(KC_LSFT);
+            unregister_code16(KC_RSFT);
+            uprintf("(\n");
+            register_code16(DE_LPRN);
+            unregister_code16(DE_LPRN);
+            return false;
+        }
+        break;
+    case TD(TD_CR): // lsft + rsft --> )
+        rsft_depressed = record->event.pressed;
+        if (lsft_depressed && rsft_depressed) {
+            unregister_code16(KC_RSFT);
+            unregister_code16(KC_LSFT);
+            uprintf(")\n");
+            register_code16(DE_RPRN);
+            unregister_code16(DE_RPRN);
+            return false;
+        }
+        break;
+
+    case KC_LCTL: // rsft + lctl --> {
+        lctl_depressed = record->event.pressed;
+        if (rsft_depressed && lctl_depressed) {
+            unregister_code(KC_LCTL);
+            unregister_code(KC_RSFT);
+            register_code16(DE_LCBR);
+            unregister_code16(DE_LCBR);
+            return false;
+        }
+        break;
+    case KC_RCTL: // lsft + rctl --> }
+        rctl_depressed = record->event.pressed;
+        if (lsft_depressed && rctl_depressed) {
+            unregister_code(KC_RCTL);
+            unregister_code(KC_LSFT);
+            register_code16(DE_RCBR);
+            unregister_code16(DE_RCBR);
+            return false;
+        }
+        break;
+
+    case TD(TD_TG_L): // rsft + ralt --> [
+        lalt_depressed = record->event.pressed;
+        if (rsft_depressed && lalt_depressed) {
+            unregister_code(KC_LALT);
+            unregister_code(KC_RSFT);
+            register_code16(DE_LBRC);
+            unregister_code16(DE_LBRC);
+            return false;
+        }
+        break;
+    case TD(TD_TG_R): // lsft + ralt --> ]
+        ralt_depressed = record->event.pressed;
+        if (lsft_depressed && ralt_depressed) {
+            unregister_code(KC_RALT);
+            unregister_code(KC_LSFT);
+            register_code16(DE_RBRC);
+            unregister_code16(DE_RBRC);
+            return false;
+        }
+        break;
     }
+    //uprintf("kc %d %d\n", keycode, record->event.pressed);
     return true;
 }
 
 // tap dance input / double tap input
-
-enum {
-    TD_CL,
-    TD_CR,
-};
 
 void dance_cl_finished(qk_tap_dance_state_t *state, void *user_data) {
     if (state->count == 1) { register_code16(KC_LSFT); }
@@ -77,4 +208,6 @@ void dance_cr_reset(qk_tap_dance_state_t *state, void *user_data) {
 qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_CL]   = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_cl_finished, dance_cl_reset),
     [TD_CR]   = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_cr_finished, dance_cr_reset),
+    [TD_TG_L] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_LALT, _TG_1),
+    [TD_TG_R] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_RALT, _TG_1),
 };
