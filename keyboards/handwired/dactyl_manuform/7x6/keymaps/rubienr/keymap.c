0fa419640a5ecd2e890ef9423740d267702c6bea
Raoul Rubien <raoul.rubienr@gmail.com>
2021-12-10 20:29:11 +0100

    keymap cleanup

--- keyboards/handwired/dactyl_manuform/7x6/keymaps/rubienr/keymap.c
+++ keyboards/handwired/dactyl_manuform/7x6/keymaps/rubienr/keymap.c
@@ -2,41 +2,42 @@
 #include <split_util.h>
 #include <keymap_german.h>
 
-#define _QWERTY 0
-#define _R_MO   1
-#define _L_MO   2
+#define _QWERTY    0
+#define _TG_1      1
+#define _MO_1      2
 
-#define R_MO MO(_R_MO)
-#define L_MO MO(_L_MO)
+#define MO_1 MO(_MO_1)
+#define TG_1 TG(_TG_1)
 
+#include "init.c"
 #include "oled.c"
 #include "input.c"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {                            
     [_QWERTY] = LAYOUT_7x6(                                                               
-        KC_ESC  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   ,                                            KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  ,
-        DE_CIRC , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,                                            KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , DE_PLUS ,
-        KC_TAB  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    ,                                            KC_Z    , KC_U    , KC_I    , KC_O    , KC_P    , KC_DEL  ,
-       TD(TD_CL), KC_A    , KC_S    , KC_D    , KC_F    , KC_G    ,                                            KC_H    , KC_J    , KC_K    , KC_L    , DE_HASH ,TD(TD_CR), 
-        KC_LCTL , KC_Y    , KC_X    , KC_C    , KC_V    , KC_B    ,                                            KC_N    , KC_M    , DE_COMM , DE_DOT  , DE_MINS , KC_RCTL ,
-                            KC_LBRC , KC_LPRN ,                                                                                    KC_RPRN , KC_RBRC , 
+        KC_ESC  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   ,                                            KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , DE_SS   ,
+        DE_CIRC , DE_1    , DE_2    , DE_3    , DE_4    , DE_5    ,                                            DE_6    , DE_7    , DE_8    , DE_9    , DE_0    , DE_ACUT ,
+        KC_TAB  , DE_Q    , DE_W    , DE_E    , DE_R    , DE_T    ,                                            DE_Z    , DE_U    , DE_I    , DE_O    , DE_P    , DE_PLUS ,
+       TD(TD_CL), DE_A    , DE_S    , DE_D    , DE_F    , DE_G    ,                                            DE_H    , DE_J    , DE_K    , DE_L    , DE_HASH ,TD(TD_CR),
+       KC_LCTL  , DE_Y    , DE_X    , DE_C    , DE_V    , DE_B    ,                                            DE_N    , DE_M    , DE_COMM , DE_DOT  , DE_MINS , KC_RCTL ,
+                            DE_LABK ,TD(TD_TG_L),                                                                                 TD(TD_TG_R), KC_DEL  ,
                                                 KC_SPC  , KC_TAB  ,                                            KC_BSPC , KC_ENT  ,
-                                                                    KC_LALT , L_MO    ,    R_MO    , KC_RALT ,
-                                                                    CK_UCIS , XXXXXXX ,    XXXXXXX , DE_ACUT
-    ),                                                                                    
-    [_R_MO] = LAYOUT_7x6(                                                                 
-        _______ , _______ , _______ , _______ , _______ , _______ ,                                            RESET   , KC_INS  , KC_HOME , KC_PGUP , KC_F11  , KC_F12  ,
-        _______ , _______ , _______ , _______ , _______ , _______ ,                                            XXXXXXX , KC_DEL  , KC_END  , KC_PGDN , XXXXXXX , XXXXXXX ,
-        _______ , _______ , _______ , _______ , _______ , _______ ,                                            XXXXXXX , XXXXXXX , KC_UP   , XXXXXXX , XXXXXXX , _______ ,
-        _______ , _______ , _______ , _______ , _______ , _______ ,                                            XXXXXXX , KC_LEFT , KC_DOWN , KC_RGHT , XXXXXXX , _______ ,
-        _______ , _______ , _______ , _______ , _______ , _______ ,                                            XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , _______ ,
-                            _______ , _______ ,                                                                                    XXXXXXX , XXXXXXX ,
+                                                                       MO_1 , XXXXXXX ,    KC_LEFT , KC_UP ,
+                                                                    XXXXXXX , CK_UCIS ,    KC_DOWN , KC_RGHT
+    ),
+    [_TG_1] = LAYOUT_7x6(                                                                 
+        _______ , _______ , _______ , _______ , _______ , _______ ,                                            _______ , _______ , _______ , _______ , _______ , _______ ,
+        _______ , _______ , _______ , _______ , _______ , _______ ,                                            _______ , _______ , _______ , _______ , _______ , _______ ,
+        _______ , _______ , _______ , _______ , _______ , _______ ,                                            _______ , _______ , _______ , _______ , _______ , _______ ,
+        _______ , _______ , _______ , _______ , _______ , _______ ,                                            _______ , _______ , _______ , _______ , _______ , _______ ,
+        _______ , _______ , _______ , _______ , _______ , _______ ,                                            _______ , _______ , _______ , _______ , _______ , _______ ,
+                            _______ , _______ ,                                                                                    _______ , _______ ,
                                                 _______ , _______ ,                                            _______ , _______ ,
-                                                                    _______ , _______ ,    _______ , _______ ,
-                                                                    _______ , _______ ,    _______ , _______
-    ),                                                                                    
-    [_L_MO] = LAYOUT_7x6(                                                                 
-        TERM_OFF, TERM_ON , XXXXXXX , XXXXXXX , XXXXXXX , RESET   ,                                            _______ , KC_PSCR , KC_SLCK , KC_PAUS , _______ , _______ , 
+                                                                    _______ , _______ ,    KC_HOME , KC_PGUP ,
+                                                                    _______ , _______ ,    KC_PGDN , KC_END
+    ),
+    [_MO_1] = LAYOUT_7x6(                                                                 
+        TERM_OFF, TERM_ON , XXXXXXX , XXXXXXX , XXXXXXX , RESET   ,                                            _______ , KC_PSCR , KC_SLCK , KC_PAUS , _______ , KC_F12  ,
         DEBUG   , XXXXXXX , RGB_SPD , RGB_SPI , XXXXXXX , XXXXXXX ,                                            _______ , _______ , _______ , _______ , _______ , _______ ,
         _______ , XXXXXXX , RGB_VAD , RGB_VAI , XXXXXXX , XXXXXXX ,                                            _______ , _______ , _______ , _______ , _______ , _______ ,
         _______ , AU_TOG  , RGB_SAD , RGB_SAI , XXXXXXX , XXXXXXX ,                                            _______ , _______ , _______ , _______ , _______ , _______ ,
@@ -44,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             RGB_RMOD, RGB_MOD ,                                                                                    _______ , _______ ,
                                                 _______ , _______ ,                                            _______ , _______ ,
                                                                     _______ , _______ ,    _______ , _______ ,
-                                                                    _______ , _______ ,    _______ , _______
-    ),                                                                                    
-//  [_UNI] = LAYOUT_7x6(                                                                  
+                                                                    _______ , RESET   ,      RESET , _______
+    ),
+//  [_L_DSBL] = LAYOUT_7x6(                                                                  
 //      XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                                            XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
 //      XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                                            XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
 //      XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                                            XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
@@ -55,8 +56,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //                                              _______ , _______ ,                                            _______ , _______ ,
 //                                                                  _______ , _______ ,    _______ , _______ ,
 //                                                                  _______ , _______ ,    _______ , _______
-//  ),                                                                                    
-//  [_L_MO] = LAYOUT_7x6(                                                                 
+//  ),                                                                                 
+//  [_L_TRNS] = LAYOUT_7x6(                                                                 
 //      _______ , _______ , _______ , _______ , _______ , _______ ,                                            _______ , _______ , _______ , _______ , _______ , _______ ,
 //      _______ , _______ , _______ , _______ , _______ , _______ ,                                            _______ , _______ , _______ , _______ , _______ , _______ ,
 //      _______ , _______ , _______ , _______ , _______ , _______ ,                                            _______ , _______ , _______ , _______ , _______ , _______ ,
@@ -68,15 +69,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //                                                                  _______ , _______ ,    _______ , _______
 //  ),
 };
-
-
-void board_init(void) {
-#ifdef OLED_ENABLE
-    // B9 is configured as I2C1_SDA in the board file; that function must be disabled before using B7 as I2C1_SDA.
-    // https://github.com/qmk/qmk_firmware/blob/master/keyboards/handwired/onekey/blackpill_f401/blackpill_f401.c
-    // https://github.com/qmk/qmk_firmware/pull/10322
-    setPinInputHigh(B9);
-#endif
-}
-
-
